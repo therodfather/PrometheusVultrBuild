9cb0029eba11066c15a53f1ee2e89bd52b12af3a
therodfather <34672275+therodfather@users.noreply.github.com>
2020-08-03 13:58:13 -0400

    Update prom.tf

--- prom.tf
+++ prom.tf
@@ -4,6 +4,11 @@ provider "vultr" {
   retry_limit = 3
 }
 
+resource "vultr_ssh_key" "my_ssh_key" {
+  name = "my_ssh_key"
+  ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC161MOpqIM+HRg4wVFhgmM3tVcmm0dFx5VTF3EZSHWkOY6qJrYQsUtEGIpDLWjG2AzXEZdDwCjmY+mCceJr9af+GoYNHUILnrCNWwP0hn1gSjGT8Vsr+tlt7E6RTQBvLSAxYK049cDQAGXim6TWHqzv/9nlm18sI5++5g6ytzqSR7yc7+SkD8fruKpT3YCoz26DdlN6NjbgKVdBniD5ol9WEArc8tguCrvseotJHYSUHNYhF4smbd39/tx42VyFpf3un21yJ7jyf0vgM5X6xRIJCDz7TWd4K80+cisVeclJhRJZErJnfOEDgW/XRs/ExK2o/Twe1Rm5/urAS8T0HxN rodfather@DESKTOP-7TDO7G3"
+}
+
 module "vpc" {
   source = "git::https://github.com/therodfather/Scripts.git"
 }
